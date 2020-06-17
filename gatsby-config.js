8fa3e5b8515e9bb1eea562e774e94a97ee17f0e7
David Garvie <garvie88@gmail.com>
2020-06-17 17:42:39 +0100

    Add support to specify contentful environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,8 @@ require('dotenv').config({
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  host: process.env.CONTENTFUL_HOST
+  host: process.env.CONTENTFUL_HOST,
+  environment: process.env.CONTENTFUL_ENVIRONMENT
 }
 
 const { spaceId, accessToken } = contentfulConfig
